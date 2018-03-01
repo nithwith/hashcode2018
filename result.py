475a6a44247a400903c5e67cb2c68832ca67c312
tetopowa <theo.lacroix@live.fr>
2018-03-01 19:35:24 +0100

    ff

--- result.py
+++ result.py
@@ -1,1 +1,10 @@
-print("hello world")
+with open('b_should_be_easy.in') as fp:
+    first_ligne  = fp.readline().split(' ')
+    R = first_ligne[0]
+    C = first_ligne[1]
+    F = first_ligne[2]
+    N = first_ligne[3]
+    B = first_ligne[4]
+    T = first_ligne[5]
+    # for line in fp:
+        # print line
